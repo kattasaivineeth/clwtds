1ad9d36aa8e0844d26a7516c54eff4722c1b4efd
Noob Coder <72628797+CodeBreaking-byte@users.noreply.github.com>
2021-11-03 23:20:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,69 @@
 
+
+const moreContent0 = document.querySelector(".hide-0");
+const seeMoreBtn0 = document.querySelector(".see-more-0");
+moreContent0.style.display = "none";
+seeMoreBtn0.addEventListener('click', () => {
+     
+    // moreContent.classList.seeMoreBtn('see-more0');
+    if(seeMoreBtn0.innerHTML === 'see more') {
+        seeMoreBtn0.innerHTML = 'see less'; 
+        moreContent0.style.display = "inline";       
+    }
+    else {
+        seeMoreBtn0.innerHTML = 'see more';        
+        moreContent0.style.display = "none";
+    }
+});
+
+
+const moreContent1 = document.querySelector(".hide-1");
+const seeMoreBtn1 = document.querySelector(".see-more-1");
+moreContent1.style.display = "none";
+seeMoreBtn1.addEventListener('click', () => {
+     
+    // moreContent.classList.seeMoreBtn('see-more1');
+    if(seeMoreBtn1.innerHTML === 'see more') {
+        seeMoreBtn1.innerHTML = 'see less'; 
+        moreContent1.style.display = "inline";       
+    }
+    else {
+        seeMoreBtn1.innerHTML = 'see more';        
+        moreContent1.style.display = "none";
+    }
+});
+
+
+const moreContent2 = document.querySelector(".hide-2");
+const seeMoreBtn2 = document.querySelector(".see-more-2");
+moreContent2.style.display = "none";
+seeMoreBtn2.addEventListener('click', () => {
+     
+    // moreContent.classList.seeMoreBtn('see-more2');
+    if(seeMoreBtn2.innerHTML === 'see more') {
+        seeMoreBtn2.innerHTML = 'see less'; 
+        moreContent2.style.display = "inline";       
+    }
+    else {
+        seeMoreBtn2.innerHTML = 'see more';        
+        moreContent2.style.display = "none";
+    }
+});
+
+
+const moreContent3 = document.querySelector(".hide-3");
+const seeMoreBtn3 = document.querySelector(".see-more-3");
+moreContent3.style.display = "none";
+seeMoreBtn3.addEventListener('click', () => {
+     
+    // moreContent.classList.seeMoreBtn('see-more3');
+    if(seeMoreBtn3.innerHTML === 'see more') {
+        seeMoreBtn3.innerHTML = 'see less'; 
+        moreContent3.style.display = "inline";       
+    }
+    else {
+        seeMoreBtn3.innerHTML = 'see more';        
+        moreContent3.style.display = "none";
+    }
+});
+
